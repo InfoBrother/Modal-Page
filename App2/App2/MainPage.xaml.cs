10ff427408f0614658da1820384572f8b8abbc39
InfoBrother <Infobrother33@gmail.com>
2018-02-19 12:32:58 +0400

    Initial commit

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace App2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        async void onclicked (object sender, EventArgs e)
+        {
+            var disp = new displaypage();
+            await Navigation.PushModalAsync(disp);
+        }
+
+    }
+}
---
+++ App2/App2/displaypage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class displaypage : ContentPage
+	{
+		public displaypage ()
+		{
+			InitializeComponent ();
+		}
+
+        async void OnDismissButtonClicked(object sender, EventArgs args)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+    }
+}
